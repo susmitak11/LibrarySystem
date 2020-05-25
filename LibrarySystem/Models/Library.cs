37172783d59d09f29cff1795e74b4df31ae78172
Susmita <mishrasusmita560@gmail.com>
2020-05-25 18:18:36 +1200

    added readme file and added comments

--- LibrarySystem/Models/Library.cs
+++ LibrarySystem/Models/Library.cs
@@ -7,6 +7,7 @@ namespace LibrarySystem.Models
 {
     public class Library
     {
+        //the attributes that need to be filled
         public int Id { get; set; }
         public string Name { get; set; }
         public int LibraryPopulation { get; set; }
